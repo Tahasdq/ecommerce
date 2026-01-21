2bfd4655d86d35c2165e41950e5873305e407ea9
Tahasdq <64333343+Tahasdq@users.noreply.github.com>
2026-01-21 23:38:58 +0500

    Updated middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -31,5 +31,5 @@ export  function middleware(request: NextRequest) {
 
 // ✅ This matcher skips internal/static routes automatically
 export const config = {
-  matcher: ['/((?!_next|favicon.ico|api).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\..*).*)'],
 }
